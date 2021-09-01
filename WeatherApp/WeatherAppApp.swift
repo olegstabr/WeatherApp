37024a58d86a0c145fbbce5e43162fdcecd5c8e1
oleg stabrovsky <olegstabr@gmail.com>
2021-09-01 18:57:12 +0700

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Олег Стабровский on 01.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
