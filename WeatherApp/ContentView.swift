10ab68b6fbdbcd4f2cac37e414c3f41e34ed6196
oleg stabrovsky <olegstabr@gmail.com>
2021-09-02 22:04:01 +0700

    Add nav view with list; Add show weather forecast by daily; Add enter location name;

--- WeatherApp/ContentView.swift
+++ WeatherApp/ContentView.swift
@@ -9,17 +9,62 @@ import SwiftUI
 import CoreLocation
 
 struct ContentView: View {
+	@State private var location = ""
+	@State var forecast: Forecast?
+	let dateFormatter = DateFormatter()
+	
+	init() {
+		dateFormatter.dateFormat = "E, MMM, d"
+	}
+	
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-		json()
+		NavigationView {
+			VStack {
+				HStack {
+					TextField("Enter location", text: $location)
+						.textFieldStyle(RoundedBorderTextFieldStyle())
+					Button(action: {
+						getWeatherForecast(for: location)
+					}, label: {
+						Image(systemName: "magnifyingglass.circle")
+							.font(.title3)
+					})
+				}
+				if let forecast = forecast {
+					List(forecast.daily, id: \.dt) { day in
+						VStack(alignment: .leading) {
+							Text("\(dateFormatter.string(from: day.dt))")
+								.fontWeight(.bold)
+							HStack(alignment: .top) {
+								Image(systemName: "hourglass")
+									.font(.title)
+									.frame(width: 50, height: 50)
+									.background(RoundedRectangle(cornerRadius: 10)
+													.fill(Color.green))
+								VStack(alignment: .leading) {
+									Text("Description: \(String(describing: day.weather.first!.description.capitalized))")
+									Text("High: \(day.temp.max)")
+									Text("Low: \(day.temp.min)")
+									Text("Humidity: \(day.humidity)")
+									Text("Clouds: \(day.clouds)")
+									Text("POP: \(day.pop)")
+								}
+							}
+						}
+					}
+					.listStyle(PlainListStyle())
+				} else {
+					Spacer()
+				}
+			}
+			.padding(.horizontal)
+			.navigationTitle("Weather App")
+		}
     }
 	
-	func json() -> some View {
+	func getWeatherForecast(for location: String) {
 		let apiService = APIService.shared
-		let dateFormatter = DateFormatter()
-		dateFormatter.dateFormat = "E, MMM, d"
-		CLGeocoder().geocodeAddressString("Novosibirsk") { (placemarks, error) in
+		CLGeocoder().geocodeAddressString(location) { (placemarks, error) in
 			if let error = error {
 				print(error.localizedDescription)
 			}
@@ -30,19 +75,20 @@ struct ContentView: View {
 				apiService.getJSON(urlString: url) { (result: Result<Forecast, APIService.APIError>) in
 							switch result {
 							case .success(let forecast):
-								print("Lat: \(forecast.lat)")
-								print("Lon: \(forecast.lon)")
-								print("Timezone: \(forecast.timezone)")
-								for day in forecast.daily {
-									print(dateFormatter.string(from: day.dt))
-									print("  Max: \(day.temp.max)")
-									print("  Min: \(day.temp.min)")
-									print("  Humidity: \(day.humidity)")
-									print("  Description: \(String(describing: day.weather.first!.description))")
-									print("  Clouds: \(day.clouds)")
-									print("  Pop: \(day.pop)")
-									print("  IconURL: \(String(describing: day.weather.first!.weatherIconURL))")
-								}
+								self.forecast  = forecast
+//								print("Lat: \(forecast.lat)")
+//								print("Lon: \(forecast.lon)")
+//								print("Timezone: \(forecast.timezone)")
+//								for day in forecast.daily {
+//									print(dateFormatter.string(from: day.dt))
+//									print("  Max: \(day.temp.max)")
+//									print("  Min: \(day.temp.min)")
+//									print("  Humidity: \(day.humidity)")
+//									print("  Description: \(String(describing: day.weather.first!.description))")
+//									print("  Clouds: \(day.clouds)")
+//									print("  Pop: \(day.pop)")
+//									print("  IconURL: \(String(describing: day.weather.first!.weatherIconURL))")
+//								}
 							case .failure(let apiError):
 								switch apiError {
 								case .error(let errorString):
@@ -52,7 +98,6 @@ struct ContentView: View {
 						}
 			}
 		}
-		return Text("")
 	}
 }
 
