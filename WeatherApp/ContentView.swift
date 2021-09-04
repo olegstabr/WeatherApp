cb3514f3e545b53d8c08a2a2f56c7af373ba595d
oleg stabrovsky <olegstabr@gmail.com>
2021-09-04 08:21:25 +0700

    Add alert by error; Add progress view by loading data from api

--- WeatherApp/ContentView.swift
+++ WeatherApp/ContentView.swift
@@ -12,56 +12,79 @@ struct ContentView: View {
 	@StateObject private var forecastListVM = ForecastListViewModel()
 	
     var body: some View {
-		NavigationView {
-			VStack {
-				Picker(selection: $forecastListVM.system, label: Text("System")) {
-					Text("°C").tag(0)
-					Text("°F").tag(1)
-				}
-				.pickerStyle(SegmentedPickerStyle())
-				.frame(width: 200)
-				.padding(.vertical)
-				HStack {
-					TextField("Enter location", text: $forecastListVM.location)
-						.textFieldStyle(RoundedBorderTextFieldStyle())
-					Button(action: {
-						forecastListVM.getWeatherForecast()
-					}, label: {
-						Image(systemName: "magnifyingglass.circle")
-							.font(.title3)
-					})
-				}
-				List(forecastListVM.forecasts, id: \.day) { day in
-					VStack(alignment: .leading) {
-						Text("\(day.day)")
-							.fontWeight(.bold)
-						HStack(alignment: .center) {
-							WebImage(url: day.weatherIconURL)
-								.resizable()
-								.placeholder {
-									Image(systemName: "hourglass")
+		ZStack {
+			NavigationView {
+				VStack {
+					Picker(selection: $forecastListVM.system, label: Text("System")) {
+						Text("°C").tag(0)
+						Text("°F").tag(1)
+					}
+					.pickerStyle(SegmentedPickerStyle())
+					.frame(width: 200)
+					.padding(.vertical)
+					HStack {
+						TextField("Enter location", text: $forecastListVM.location)
+							.textFieldStyle(RoundedBorderTextFieldStyle())
+						Button(action: {
+							forecastListVM.getWeatherForecast()
+						}, label: {
+							Image(systemName: "magnifyingglass.circle")
+								.font(.title3)
+						})
+					}
+					List(forecastListVM.forecasts, id: \.day) { day in
+						VStack(alignment: .leading) {
+							Text("\(day.day)")
+								.fontWeight(.bold)
+							HStack(alignment: .center) {
+								WebImage(url: day.weatherIconURL)
+									.resizable()
+									.placeholder {
+										Image(systemName: "hourglass")
+									}
+									.scaledToFit()
+									.frame(width: 100)
+								VStack(alignment: .leading) {
+									Text("\(day.overview)")
+									HStack {
+										Text("\(day.high)")
+										Text("\(day.low)")
+									}
+									HStack {
+										Text("\(day.clouds)")
+										Text("\(day.pop)")
+									}
+									Text("\(day.humidity)")
 								}
-								.scaledToFit()
-								.frame(width: 100)
-							VStack(alignment: .leading) {
-								Text("\(day.overview)")
-								HStack {
-									Text("\(day.high)")
-									Text("\(day.low)")
-								}
-								HStack {
-									Text("\(day.clouds)")
-									Text("\(day.pop)")
-								}
-								Text("\(day.humidity)")
 							}
 						}
 					}
+					.listStyle(PlainListStyle())
 				}
-				.listStyle(PlainListStyle())
+				.padding(.horizontal)
+				.navigationTitle("Oleg Weather")
+				.alert(item: $forecastListVM.appError) { appAlert in
+					Alert(title: Text("Error"), message: Text("""
+						\(appAlert.errorString)
+						Please try again later
+						"""
+						))
+				}
 			}
-			.padding(.horizontal)
-			.navigationTitle("Oleg Weather")
+			if forecastListVM.isLoading {
+				ZStack {
+					Color(.white)
+						.opacity(0.5)
+						.ignoresSafeArea()
+					ProgressView("Fetching Weather")
+						.padding()
+						.background(
+							RoundedRectangle(cornerRadius: 10)
+								.fill(Color(.systemBackground))
+						)
+						.shadow(radius: 10)
+				}
+			}
 		}
     }
 }
--- WeatherApp/ForecastListViewModel.swift
+++ WeatherApp/ForecastListViewModel.swift
@@ -10,7 +10,14 @@ import CoreLocation
 import SwiftUI
 
 class ForecastListViewModel: ObservableObject {
+	struct AppError: Identifiable {
+		let id = UUID().uuidString
+		let errorString: String
+	}
+	
+	var appError: AppError?
 	@Published var forecasts: [ForecastViewModel] = []
+	@Published var isLoading = false
 	@AppStorage("location") var location = ""
 	@AppStorage("system") var system = 0 {
 		didSet {
@@ -27,9 +34,12 @@ class ForecastListViewModel: ObservableObject {
 	}
 	
 	func getWeatherForecast() {
+		isLoading = true
 		let apiService = APIService.shared
 		CLGeocoder().geocodeAddressString(location) { (placemarks, error) in
 			if let error = error {
+				self.isLoading = false
+				self.appError = AppError(errorString: error.localizedDescription)
 				print(error.localizedDescription)
 			}
 			
@@ -37,18 +47,21 @@ class ForecastListViewModel: ObservableObject {
 			   let lon = placemarks?.first?.location?.coordinate.longitude {
 				let url = "https://api.openweathermap.org/data/2.5/onecall?lat=\(lat)&lon=\(lon)&exclude=current,minutely,hourly,alerts&appid=3863d70f79a9d670d05ef83564bb7648"
 				apiService.getJSON(urlString: url) { (result: Result<Forecast, APIService.APIError>) in
-							switch result {
-							case .success(let forecast):
-								DispatchQueue.main.async {
-									self.forecasts = forecast.daily.map { ForecastViewModel(forecast: $0, system: self.system) }
-								}
-							case .failure(let apiError):
-								switch apiError {
-								case .error(let errorString):
-									print(errorString)
-								}
-							}
+					switch result {
+					case .success(let forecast):
+						DispatchQueue.main.async {
+							self.isLoading = false
+							self.forecasts = forecast.daily.map { ForecastViewModel(forecast: $0, system: self.system) }
 						}
+					case .failure(let apiError):
+						switch apiError {
+						case .error(let errorString):
+							self.isLoading = false
+							self.appError = AppError(errorString: errorString)
+							print(errorString)
+						}
+					}
+				}
 			}
 		}
 	}
